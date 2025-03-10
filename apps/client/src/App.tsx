0955242b534a4bac2a5f97e347488e9020b712c3
MAOFU <2148334528@qq.com>
2025-03-10 19:21:35 +0800

    feat: 【client】antd 配置中文

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,16 +1,24 @@
 import '@/assets/scss/reset.scss';
 import '@/assets/scss/theme-css-variables.scss';
+import { ConfigProvider, ConfigProviderProps } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import 'normalize.css';
+import { useState } from 'react';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import store from './store';
 
+type Locale = ConfigProviderProps['locale'];
+
 function App() {
+  const [locale] = useState<Locale>(zhCN);
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <ConfigProvider locale={locale}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ConfigProvider>
   );
 }
 
