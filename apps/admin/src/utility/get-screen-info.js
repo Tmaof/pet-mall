f16325a33d68fd965eba8138a79d2653baa78aec
MAOFU <2148334528@qq.com>
2025-04-23 15:14:26 +0800

    fix: 【admin】获取屏幕宽度时，pc端优先

--- apps/admin/src/utility/get-screen-info.js
+++ apps/admin/src/utility/get-screen-info.js
@@ -1,10 +1,10 @@
 import { reactive } from 'vue'
-const screenInfo = reactive({ isMobile: true, width: 0 })
+const screenInfo = reactive({ isMobile: false, width: 1080 })
 function getInfo() {
   screenInfo.width = window.innerWidth
   screenInfo.isMobile = window.innerWidth < 768
 }
-window.addEventListener('load', getInfo)
+window.addEventListener('DOMContentLoaded', getInfo)
 window.addEventListener('resize', getInfo)
 export default function getScreenInfo() {
   return screenInfo
